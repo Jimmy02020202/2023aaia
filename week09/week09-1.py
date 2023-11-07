438a76d1f3c811f263e2e015845883417a0fcff8
Jimmy02020202 <xlin35858@gmail.com>
2023-11-07 10:42:54 +0800

    week09

--- week09/week09-1.py
+++ week09/week09-1.py
@@ -1,9 +1,3 @@
-# 若陣列a的長度不一樣呢?
-a = [1, 3, 5, 7, 9, 11, 13, 15, 17]
+a = [1, 3, 5, 7, 9]
 for i in range(5):
-  print('i是', i,'這時a[i]的值是',a[i])
-
-print('比較好的寫法，這個range(N)')
-N = len(a)
-for i in range(N):
-  print('i是', i,'這時a[i]的值是',a[i])
+  print('a[i]的值是',a[i])
